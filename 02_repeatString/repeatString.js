2322bc68c461dea7690a696bd55c372bb87143c3
sinsect <sin@odin.proj>
2022-06-15 16:23:21 -0400

    create repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeatText, repeatCount) {
+    if (repeatCount < 0) {
+        return "ERROR";
+    }
+    let outputText = "";
+    for (let i = 0; i < repeatCount; i++) {
+        outputText += repeatText;
+    }
+    return outputText;
 };
 
 // Do not edit below this line
