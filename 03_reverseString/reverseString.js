9e72c0b9c1782946590407d8c7f8f0a00b06e6c5
sinsect <sin@odin.proj>
2022-06-15 16:29:48 -0400

    create reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringIn) {
+    let stringOut = "";
+    for (let i = 1; i <= stringIn.length; i++) {
+        stringOut += stringIn.charAt(stringIn.length - i);
+    }
+    return stringOut;
 };
 
 // Do not edit below this line
