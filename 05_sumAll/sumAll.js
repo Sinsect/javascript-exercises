f90fec3b95705bc7f3e2399859b3dad850886b02
sinsect <sin@odin.proj>
2022-06-16 16:22:20 -0400

    create sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (int1, int2) {
+    let startInt = 0;
+    let endInt = 0;
+    if (int1 >= 0 && int2 >= 0 &&
+        typeof (int1) == "number" && typeof (int2) == "number") {
+        if (int1 < int2) {
+            startInt = int1;
+            endInt = int2;
+        }
+        else {
+            startInt = int2;
+            endInt = int1;
+        }
+        let sumInt = startInt;
+        for (let i = startInt + 1; i <= endInt; i++) {
+            sumInt += i;
+        }
+        return sumInt;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
