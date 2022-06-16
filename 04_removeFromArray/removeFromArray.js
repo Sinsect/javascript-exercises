035c23f83270267ff851879a0769aaf6f5c06d05
sinsect <sin@odin.proj>
2022-06-16 16:03:05 -0400

    create removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arrIn, ...argsToRemove) {
+    if (argsToRemove.length > 0) {
+        for (let i = 0; i < argsToRemove.length; i++) {
+            for (let j = 0; j < arrIn.length; j++) {
+                if (arrIn[j] == argsToRemove[i] && 
+                    typeof(arrIn[j]) == typeof(argsToRemove[i])) {
+                    arrIn.splice(j, 1);
+                }
+            }
+        }
+    }
+    return arrIn;
 };
 
 // Do not edit below this line
