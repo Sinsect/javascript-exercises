978438abfec205bb3abeda8f44f9585c348f1661
sinsect <sin@odin.proj>
2022-06-29 17:08:52 -0400

    create basic calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+	nums.forEach(num => {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let total = 1;
+	nums.forEach(num => {
+    total *= num;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  let result = 1;
+	for (i = 0; i < pow; i++) {
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	for (i = 2; i <= num; i++) {
+    console.log('loop run ' + i + ' times');
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
